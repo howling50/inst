6966cbdf71919e5a98f07bf3f5c361d82c728116
howling50 <95032680+howling50@users.noreply.github.com>
2021-11-26 18:26:21 +0200

    Create k.sh

---
+++ k.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+sudo pacman -S yay --noconfirm --needed
+sudo systemctl stop cups
+sudo systemctl disable cups.service cups.socket cups.path
+sudo balooctl disable
+sudo rm -rf ~/./local/share/baloo
+sudo pacman -R yakuake --noconfirm --needed
+sudo pacman -S jdk-openjdk --noconfirm --needed
+sudo pacman -S wine-gecko --noconfirm --needed
+sudo pacman -S vim --noconfirm --needed
+sudo pacman -S wine-mono --noconfirm --needed
+sudo pacman -S winetricks --noconfirm --needed
+sudo pacman -S gufw --noconfirm --needed
+sudo pacman -S proxychains --noconfirm --needed
+sudo pacman -S tor --noconfirm --needed
+sudo pacman -S pkgconf --noconfirm --needed
+sudo pacman -S mousepad --noconfirm --needed
+sudo pacman -S terminator --noconfirm --needed
+sudo pacman -S gamemode --noconfirm --needed
+sudo pacman -S hardinfo --noconfirm --needed
+sudo pacman -S audacious --noconfirm --needed
+sudo pacman -S leafpad --noconfirm --needed
+sudo pacman -S lutris --noconfirm --needed
+sudo pacman -S neofetch --noconfirm --needed
+sudo pacman -S net-tools --noconfirm --needed
+sudo pacman -S rkhunter --noconfirm --needed
+sudo pacman -S unrar --noconfirm --needed
+sudo pacman -S gimp --noconfirm --needed
+sudo pacman -S vlc --noconfirm --needed
+sudo pacman -R onlyoffice-desktopeditors --noconfirm --needed
+sudo pacman -S libreoffice-fresh --noconfirm --needed
+sudo pacman -S celluloid --noconfirm --needed
+yay -S --noconfirm stacer
+yay -S --noconfirm brave-bin
+yay -S --noconfirm mangohud
+yay -S --noconfirm mangohud-common
+yay -S --noconfirm ttf-msfonts
+yay -S --noconfirm eiskaltdcpp-qt
+yay -S --noconfirm zenmap
+yay -S --noconfirm konsave    
+sudo systemctl enable tor.service
+sudo systemctl start tor.service
+sudo systemctl start ufw
+sudo ufw enable
+konsave -i ~/Downloads/inst/kde.knsv
+sleep 1
+konsave -a kde
