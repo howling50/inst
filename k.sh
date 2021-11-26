3e765757209b29a09187f2f5cc278948849c163e
howling50 <95032680+howling50@users.noreply.github.com>
2021-11-27 01:54:52 +0200

    Update k.sh

--- k.sh
+++ k.sh
@@ -53,6 +53,6 @@ sudo systemctl enable tor.service
 sudo systemctl start tor.service
 sudo systemctl start ufw
 sudo ufw enable
-konsave -i ~/Downloads/inst/kde1.knsv
+konsave -i ~/Downloads/inst/kde.knsv
 sleep 1
-konsave -a kde1
+konsave -a kde
