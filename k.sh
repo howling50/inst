e9fee9c5124e547eb7da54db4067e83c7dc84b37
howling50 <95032680+howling50@users.noreply.github.com>
2021-12-17 02:17:38 +0200

    Update k.sh

--- k.sh
+++ k.sh
@@ -11,6 +11,7 @@ sudo cp ~/Downloads/inst/.bashrc ~/.bashrc
 sudo pacman -S yay --noconfirm --needed
 sudo systemctl stop cups
 sudo systemctl disable cups.service cups.socket cups.path
+sudo balooctl stop
 sudo balooctl disable
 sudo rm -rf ~/./local/share/baloo
 curl -O https://blackarch.org/strap.sh
