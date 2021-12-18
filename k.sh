22160b2bb27cfea0e7a1cd3eadbe9a5c804d272f
howling50 <95032680+howling50@users.noreply.github.com>
2021-12-19 00:20:46 +0200

    Update k.sh

--- k.sh
+++ k.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-sudo pacman -S powerline --noconfirm --needed
+sudo swapoff -a
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo pacman -S powerline-fonts --noconfirm --needed
 sudo rm -rf /root/.zshrc
 sudo rm -rf /root/.bashrc
 sudo cp ~/Downloads/inst/.zshrc /root/.zshrc
