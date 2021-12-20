ef31dd7c377a35bc26c136494f5f681efcf5a327
howling50 <95032680+howling50@users.noreply.github.com>
2021-12-20 15:31:18 +0200

    Update k.sh

--- k.sh
+++ k.sh
@@ -53,7 +53,7 @@ sudo pacman -S kitty --noconfirm --needed
 sudo pacman -S gamemode --noconfirm --needed
 sudo pacman -S hardinfo --noconfirm --needed
 sudo pacman -S audacious --noconfirm --needed
-sudo pacman -S leafpad --noconfirm --needed
+sudo pacman -S mousepad --noconfirm --needed
 sudo pacman -S lutris --noconfirm --needed
 sudo pacman -S neofetch --noconfirm --needed
 sudo pacman -S net-tools --noconfirm --needed
