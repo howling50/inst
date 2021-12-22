363c9d75792370eabe6f540933d6ed86ba3f827e
howling50 <95032680+howling50@users.noreply.github.com>
2021-12-22 02:11:15 +0200

    Update k.sh

--- k.sh
+++ k.sh
@@ -68,13 +68,13 @@ sudo pacman -S celluloid --noconfirm --needed
 sudo pacman -S conky --noconfirm --needed
 sudo pacman -S burpsuite --noconfirm --needed
 sudo pacman -S brave-browser --noconfirm --needed
+sudo pacman -S nmap --noconfirm --needed
 yay --sudoloop --save
 yay -S --noconfirm stacer
 yay -S --noconfirm dxvk-bin
 yay -S --noconfirm key-mapper
 yay -S --noconfirm sejda-desktop
 yay -S --noconfirm nerd-fonts-fira-code
-yay -S --noconfirm eiskaltdcpp-qt
 yay -S --noconfirm ttf-meslo
 yay -S --noconfirm ttf-ms-fonts
 yay -S --noconfirm konsave
