73f7b631094e2d4be7b6856eb33821d0c6fd7a39
howling50 <95032680+howling50@users.noreply.github.com>
2021-12-23 12:58:33 +0200

    Update k.sh

--- k.sh
+++ k.sh
@@ -114,8 +114,6 @@ cp -r ~/Downloads/inst/files/* ~/.config/
 cd ~/Downloads/inst/
 git clone https://github.com/yeyushengfan258/Win11OS-kde 
 sudo bash ~/Downloads/inst/Win11OS-kde/install.sh
-git clone https://github.com/yeyushengfan258/Win11-icon-theme
-sudo bash ~/Downloads/inst/Win11-icon-theme/install.sh
 chmod +x install.sh
 sudo ./install.sh
 konsave -i ~/Downloads/inst/kde1.knsv
