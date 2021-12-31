4ccc20df1cf5b0cdcc9f696b66df47b3f904d2de
howling50 <95032680+howling50@users.noreply.github.com>
2021-12-31 22:24:19 +0200

    Update k.sh

--- k.sh
+++ k.sh
@@ -5,7 +5,7 @@
 #sudo mkswap /swapfile
 #sudo swapon /swapfile
 #echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-sudo pacman -R timeshift-autosnap-manjaro --noconfirm
+#sudo pacman -R timeshift-autosnap-manjaro --noconfirm
 sudo mkdir -p /etc/cron.minutely
 sudo rm -rf /etc/cron.d/0hourly
 sudo cp ~/Downloads/inst/mycronjobs /etc/cron.d/
@@ -21,9 +21,9 @@ chmod +x strap.sh
 sudo ./strap.sh
 sudo sed -i 's/^#Para/Para/' /etc/pacman.conf
 sudo pacman-mirrors --fasttrack && sudo pacman -Syyu --noconfirm --needed
-sudo pacman -S snapper-gui --noconfirm --needed
-sudo pacman -S snap-pac --noconfirm --needed
-sudo pacman -S snap-sync --noconfirm --needed
+#sudo pacman -S snapper-gui --noconfirm --needed
+#sudo pacman -S snap-pac --noconfirm --needed
+#sudo pacman -S snap-sync --noconfirm --needed
 sudo pacman -S powerline-fonts --noconfirm --needed
 sudo rm -rf /root/.zshrc
 sudo rm -rf /root/.bashrc
@@ -149,14 +149,14 @@ git clone https://github.com/yeyushengfan258/Win11OS-kde
 sudo bash ~/Downloads/inst/Win11OS-kde/install.sh
 git clone https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes
 # --------- Snaps ------------------------
-sudo snapper -c root create-config /
-sudo chmod a+rx /.snapshots/
-sudo sed -i 's/^ALLOW_USERS=""/ALLOW_USERS="howling"/' /etc/snapper/configs/root
-sudo sed -i 's/^TIMELINE_LIMIT_HOURLY="10"/TIMELINE_LIMIT_HOURLY="0"/' /etc/snapper/configs/root
-sudo sed -i 's/^TIMELINE_LIMIT_DAILY="10"/TIMELINE_LIMIT_DAILY="4"/' /etc/snapper/configs/root
-sudo sed -i 's/^TIMELINE_LIMIT_WEEKLY="0"/TIMELINE_LIMIT_WEEKLY="2"/' /etc/snapper/configs/root
-sudo sed -i 's/^TIMELINE_LIMIT_MONTHLY="10"/TIMELINE_LIMIT_MONTHLY="1"/' /etc/snapper/configs/root
-sudo sed -i 's/^TIMELINE_LIMIT_YEARLY="10"/TIMELINE_LIMIT_YEARLY="0"/' /etc/snapper/configs/root
+#sudo snapper -c root create-config /
+#sudo chmod a+rx /.snapshots/
+#sudo sed -i 's/^ALLOW_USERS=""/ALLOW_USERS="howling"/' /etc/snapper/configs/root
+#sudo sed -i 's/^TIMELINE_LIMIT_HOURLY="10"/TIMELINE_LIMIT_HOURLY="0"/' /etc/snapper/configs/root
+#sudo sed -i 's/^TIMELINE_LIMIT_DAILY="10"/TIMELINE_LIMIT_DAILY="4"/' /etc/snapper/configs/root
+#sudo sed -i 's/^TIMELINE_LIMIT_WEEKLY="0"/TIMELINE_LIMIT_WEEKLY="2"/' /etc/snapper/configs/root
+#sudo sed -i 's/^TIMELINE_LIMIT_MONTHLY="10"/TIMELINE_LIMIT_MONTHLY="1"/' /etc/snapper/configs/root
+#sudo sed -i 's/^TIMELINE_LIMIT_YEARLY="10"/TIMELINE_LIMIT_YEARLY="0"/' /etc/snapper/configs/root
 # ----------------------------------------------
 konsave -i ~/Downloads/inst/kde2.knsv
 sleep 1
