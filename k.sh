c9d7345b734fd721f12627d4781f30102189eb34
howling50 <95032680+howling50@users.noreply.github.com>
2022-01-02 20:11:34 +0200

    Update k.sh

--- k.sh
+++ k.sh
@@ -9,6 +9,7 @@
 sudo pacman -S caffeine-ng --noconfirm --needed
 caffeine &
 git clone https://github.com/howling50/Top-5-Bootloader-Themes
+sudo mkdir -p /etc/crontab
 sudo mkdir -p /etc/cron.minutely
 sudo cp ~/Downloads/inst/mycronjobs /etc/cron.d/
 btrfs subvol create ~/.local/share/Steam
