16459354c1fbc7c5b9541909dfa861a61775448d
howling50 <95032680+howling50@users.noreply.github.com>
2022-01-04 00:05:59 +0200

    Update checkerror.sh

--- checkerror.sh
+++ checkerror.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-echo --------------------- ERRORS FOR "$1" SEND TO ~/checkerror.txt--------------------------------------- 2>>~/checkerror.txt
+echo -----ERRORS FOR "$1" SEND TO ~/checkerror.txt----- 2>>~/checkerror.txt
 sleep 1
 bash "$1" 2>>~/checkerror.txt
