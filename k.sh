702096948abb4e4cef7f9abe0cd3400e2e1c8d2f
howling50 <95032680+howling50@users.noreply.github.com>
2022-01-04 18:42:51 +0200

    Update k.sh

--- k.sh
+++ k.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-#sudo swapoff -a
-#sudo fallocate -l 8G /swapfile
-#sudo chmod 600 /swapfile
-#sudo mkswap /swapfile
-#sudo swapon /swapfile
-#echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+##################### start with: bash checkerror.sh k.sh ##########################################
 SECONDS=0
 #sudo pacman -R timeshift-autosnap-manjaro --noconfirm
 sudo pacman -S caffeine-ng --noconfirm --needed
