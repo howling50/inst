113b8dec47947bd0d54e7cd150d8b4607c6f7cf0
howling50 <95032680+howling50@users.noreply.github.com>
2022-01-26 00:12:27 +0200

    Update k.sh

--- k.sh
+++ k.sh
@@ -164,8 +164,8 @@ sudo bash ~/Downloads/inst/Win11OS-kde/install.sh
 lookandfeeltool -a com.github.yeyushengfan258.Win11OS-dark 2>/dev/null
 sleep 2
 # --------- Snapshots ------------------------
-sudo systemctl enable grub-btrfs.path
-sudo systemctl start grub-btrfs.path
+#sudo systemctl enable grub-btrfs.path
+#sudo systemctl start grub-btrfs.path
 #sudo bash ~/Downloads/inst/scripts/1
 # ----------------------------------------------
 konsave -i ~/Downloads/inst/kde2.knsv
