f8628e38810c745ba05b3e3766a5b3d9e57d4e4f
howling50 <95032680+howling50@users.noreply.github.com>
2024-05-01 13:33:02 +0300

    Update k.sh

--- k.sh
+++ k.sh
@@ -235,6 +235,7 @@ sudo systemctl start ufw
 sudo ufw enable
 #------------------------------------
 cp -r ~/Downloads/inst/files/* ~/.config/
+cd
 cd ~/Downloads/inst/
 git clone https://github.com/yeyushengfan258/Win11OS-kde 
 sudo bash ~/Downloads/inst/Win11OS-kde/install.sh
@@ -252,7 +253,7 @@ konsave -a kde2
 cp ~/Downloads/inst/1.mp3 ~/.othercrap/1.mp3
 caffeine kill
 sudo pacman -R caffeine-ng --noconfirm
-convert ~/Downloads/inst/script/monkey.jpg monkey.png
+convert ~/Downloads/inst/script/monkey.jpg ~/Downloads/inst/script/monkey.png
 mv ~/Downloads/inst/script/monkey.png ~/.othercrap/monkey.png
 qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file://'$HOME'/.othercrap/monkey.png");}'
 #conky -c ~/.conkyrc &
