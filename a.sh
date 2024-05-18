bea5f5f4300fa1180fb3f0e9957a0e401b427570
howling50 <95032680+howling50@users.noreply.github.com>
2024-05-18 23:03:26 +0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -91,6 +91,7 @@ sudo pacman -S nmap --noconfirm --needed
 sudo pacman -S make --noconfirm --needed
 sudo pacman -S autoconf --noconfirm --needed
 sudo pacman -S flex --noconfirm --needed
+sudo pacman -S bat --noconfirm --needed
 sudo pacman -S gcc --noconfirm --needed
 sudo pacman -S patch --noconfirm --needed
 sudo pacman -S automake --noconfirm --needed
