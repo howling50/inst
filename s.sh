0ab894f8b42db9f27cd2051bc1c823fa7abb0417
howling50 <95032680+howling50@users.noreply.github.com>
2025-02-08 12:58:21 +0200

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-##################### sudo chattr -R +C ~/Downloads and git clone in ~/Downloads, then chmod +x s.sh and then ./s.sh ####################################################
+##################### git clone in ~/Downloads, then chmod +x s.sh and then ./s.sh (also dont forget to change fstab)####################################################
 #git clone https://github.com/yeyushengfan258/Win11OS-kde && sudo bash ~/Downloads/inst/Win11OS-kde/install.sh && sudo bash ~/Downloads/inst/Win11OS-kde/sddm-dark/install.sh
-#sudo visudo (Defaults timestamp_timeout=60)  patterns-games-games patterns-kde-kde_pim (xfce4-terminal --drop-down xfce4-taskmanager )  sudo zypper dup --from vlc --allow-vendor-change -y
+#sudo visudo (Defaults timestamp_timeout=60)  patterns-games-games patterns-kde-kde_pim (xfce4-terminal --drop-down xfce4-taskmanager kitty distrobox-enter -n arch)  sudo zypper dup --from vlc --allow-vendor-change -y
 #sudo zypper install -y -n conky && cp ~/Downloads/inst/conky.desktop ~/.config/autostart/conky.desktop && cp ~/Downloads/inst/.conkyrc ~/.conkyrc && conky -c ~/.conkyrc &
 #zypper in mirrorsorcerer && systemctl enable --now mirrorsorcerer
 #/etc/sysconfig/btrfsmaintenance /etc/snapper/configs/root
@@ -9,7 +9,7 @@
 #mkdir -p ~/.config/nvim && cp /home/howling/.bash* ~/ && cp /home/howling/.config/starship.toml ~/.config/ && cp /home/howling/.config/nvim/* ~/.config/nvim/
 SECONDS=0
 sudo chattr +C /home
-sudo zypper ar -cfp 90 -n VLC http://download.videolan.org/pub/vlc/SuSE/Tumbleweed/ vlc && sudo zypper ref && sudo zypper in -y -n --allow-vendor-change vlc vlc-codecs
+sudo zypper --gpg-auto-import-keys ar -cfp 90 -n VLC http://download.videolan.org/pub/vlc/SuSE/Tumbleweed/ vlc && sudo zypper ref && sudo zypper in -y -n --allow-vendor-change vlc vlc-codecs
 sudo zypper ref && sudo zypper up
 sudo zypper install -y -n systemd-zram-service &&  sudo systemctl enable --now zramswap.service
 sudo systemctl stop packagekit.service && sudo zypper remove -y PackageKit && sudo zypper addlock PackageKit
