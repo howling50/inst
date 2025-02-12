46ec1e97e4b7aa2e97fa608c32e5e61e413aaf1a
howling50 <95032680+howling50@users.noreply.github.com>
2025-02-12 16:48:16 +0200

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 ##################### git clone in ~/Downloads, then chmod +x s.sh and then ./s.sh (also dont forget to change fstab)####################################################
 #git clone https://github.com/yeyushengfan258/Win11OS-kde && sudo bash ~/Downloads/inst/Win11OS-kde/install.sh && sudo bash ~/Downloads/inst/Win11OS-kde/sddm-dark/install.sh
-#sudo visudo (Defaults timestamp_timeout=60)  patterns-games-games patterns-kde-kde_pim (xfce4-terminal --drop-down xfce4-taskmanager kitty distrobox-enter -n arch)  sudo zypper dup --from vlc --allow-vendor-change -y
+#sudo visudo (Defaults timestamp_timeout=60)  (xfce4-terminal --drop-down xfce4-taskmanager kitty distrobox-enter -n arch /etc/sysconfig/btrfsmaintenance /etc/snapper/configs/root) 
 #sudo zypper install -y -n conky && cp ~/Downloads/inst/conky.desktop ~/.config/autostart/conky.desktop && cp ~/Downloads/inst/.conkyrc ~/.conkyrc && conky -c ~/.conkyrc &
-#zypper in mirrorsorcerer && systemctl enable --now mirrorsorcerer 
-#/etc/sysconfig/btrfsmaintenance /etc/snapper/configs/root   (about:profiles, open root profile folder,Clear start up cache" )
+#zypper in mirrorsorcerer && systemctl enable --now mirrorsorcerer(patterns-games-games patterns-kde-kde_pim sudo zypper dup --from vlc --allow-vendor-change -y) (about:profiles, open root profile folder,Clear start up cache" )
 #mkdir -p ~/Media/container/arch && distrobox create -n arch -i quay.io/toolbx/arch-toolbox:latest --init --home ~/Media/container/arch && distrobox enter arch
 #mkdir -p ~/.config/nvim && cp /home/howling/.bash* ~/ && cp /home/howling/.config/starship.toml ~/.config/ && cp /home/howling/.config/nvim/* ~/.config/nvim/
 SECONDS=0
