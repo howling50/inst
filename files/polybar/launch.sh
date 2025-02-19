404875d5c612d1bb2f526e99ed47180d1f282ecc
howling50 <95032680+howling50@users.noreply.github.com>
2025-02-19 22:49:35 +0200

    Update launch.sh

--- files/polybar/launch.sh
+++ files/polybar/launch.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-
-# Terminate already running bar instances
 killall -q polybar
-
-# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch Polybar, using default config location ~/.config/polybar/config
+rm /tmp/polybar*
+rm /tmp/ipc-polybar*
 polybar main &
-
-echo "Polybar launched..."
