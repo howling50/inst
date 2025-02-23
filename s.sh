835f747db32b99cbaf0e9b755b21b43695cf1b30
howling50 <95032680+howling50@users.noreply.github.com>
2025-02-23 21:59:58 +0200

    Update s.sh

--- s.sh
+++ s.sh
@@ -14,8 +14,8 @@ sudo systemctl stop packagekit.service && sudo zypper remove -y PackageKit && su
 #sudo zypper ar -f https://download.nvidia.com/opensuse/tumbleweed/ nvidia
 #----Swap-------
 #sudo zypper install -y -n systemd-zram-service && sudo systemctl enable --now zramswap.service 
-#sudo sed -i 's/^\(GRUB_CMDLINE_LINUX_DEFAULT="\)/\1zswap.enabled=1 zswap.max_pool_percent=50 /' /etc/default/grub && sudo grub2-mkconfig -o /boot/grub2/grub.cfg
-#sudo btrfs subvol create /Swap && sudo chattr -R +C /Swap && sudo swapoff -a && sudo fallocate -l 6G /Swap/swapfile && sudo chmod 600 /Swap/swapfile && sudo mkswap /Swap/swapfile && sudo swapon /Swap/swapfile && echo '/Swap/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab && sudo swapon -a
+sudo sed -i 's/\(^GRUB_CMDLINE_LINUX_DEFAULT=".*\)"/\1 zswap.enabled=1 zswap.compressor=lz4 zswap.zpool=z3fold zswap.max_pool_percent=25 zswap.accept_threshold_percent=90"/' /etc/default/grub && sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+sudo btrfs subvol create /Swap && sudo chattr -R +C /Swap && sudo swapoff -a && sudo fallocate -l 6G /Swap/swapfile && sudo chmod 600 /Swap/swapfile && sudo mkswap /Swap/swapfile && sudo swapon /Swap/swapfile && echo '/Swap/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab && sudo swapon -a
 #------------
 sudo btrfs subvol create /Media && sudo chown $(whoami):$(whoami) /Media && sudo chmod 755 /Media && mkdir -p ~/.config/qBittorrent && mkdir -p ~/Media && mkdir -p ~/.wine && sudo mkdir -p /var/lib/flatpak && mkdir -p ~/.local/share/flatpak && sudo chattr -R +C ~/.config/qBittorrent && sudo chattr -R +C ~/Media && sudo chattr -R +C ~/.wine && sudo chattr -R +C /var/lib/flatpak && sudo chattr -R +C ~/.local/share/flatpak
 #sudo systemctl stop cups && sudo systemctl disable cups.service cups.socket cups.path
