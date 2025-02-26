5052d0757beac9c82a04ada1e7602a2089caa13f
howling50 <95032680+howling50@users.noreply.github.com>
2025-02-26 13:11:07 +0200

    Update s.sh

--- s.sh
+++ s.sh
@@ -92,7 +92,7 @@ else
 fi
 #defaults,nodatacow,noatime,autodefrag,compress=zstd,space_cache=v2,nofail 0 0
 #defaults,ssd,discard=async,noatime,compress=zstd,space_cache=v2 0 0
-#sudo zypper install -y -n xhost && xhost +local:
+#sudo zypper install -y -n xhost && xhost +local: && sudo zypper in -y -n dnf rpm-repos-openSUSE-Tumbleweed && sudo sed -i '/^\[main\]$/a fastestmirror=True\nmax_parallel_downloads=10\ndeltarpm=True\ndefaultyes=true' /etc/dnf/dnf.conf
 #sudo zypper install -y -n conky && cp ~/Downloads/inst/conky.desktop ~/.config/autostart/conky.desktop && cp ~/Downloads/inst/.conkyrc ~/.conkyrc && conky -c ~/.conkyrc &
 #zypper in mirrorsorcerer && systemctl enable --now mirrorsorcerer  xfce4-i3-workspaces-plugin  (patterns-games-games patterns-kde-kde_pim sudo zypper dup --from vlc --allow-vendor-change -y) (about:profiles, open root profile folder,Clear start up cache" ) sestatus  and /etc/selinux/config , Exec=/usr/bin/xdg-su -c /sbin/yast2  system-config-printer
 #mkdir -p ~/Media/container/arch && distrobox create -n arch -i quay.io/toolbx/arch-toolbox:latest --init --home ~/Media/container/arch && distrobox enter arch
