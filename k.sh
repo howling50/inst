669855dc0c96e54b1faa46737e78b8fd20bcd491
howling50 <95032680+howling50@users.noreply.github.com>
2025-03-28 15:52:33 +0200

    Update k.sh

--- k.sh
+++ k.sh
@@ -30,7 +30,7 @@ sudo pacman -S dnsmasq bridge-utils qemu-full virt-manager --noconfirm && sudo s
 #-----------------------------------------------------
 #sudo pacman -R parole vim --noconfirm && sudo getent group autologin > /dev/null || sudo groupadd autologin && sudo usermod -aG autologin $USER
 #sudo pacman -Rns kwalletmanager --noconfirm && sudo pacman -R elisa thunderbird vim --noconfirm && sudo pacman -S yakuake oxygen-icons gwenview okular kvantum-qt5 audiocd-kio --noconfirm --needed
-sudo pacman -S binutils nmap gcc patch fakeroot bind rofi rofi-calc yazi ventoy gimp fbreader dragon-drop perl-image-exiftool easytag shotcut hexchat --noconfirm --needed
+sudo pacman -S binutils nmap gcc patch fakeroot bind rofi rofi-calc yazi ventoy gimp fbreader dragon-drop perl-image-exiftool easytag shotcut hexchat gnome-boxes --noconfirm --needed
 sudo pacman -S qbittorrent putty aria2 bluez bluez-utils blueman fuseiso android-tools mpv vlc libreoffice-fresh viewnior cava --noconfirm --needed
 sudo pacman -S ffmpeg libfdk-aac gst-plugins-base gst-libav gst-plugins-good gst-plugins-bad gst-plugins-ugly --noconfirm --needed
 sudo pacman -S wine wine-gecko wine-mono wine-nine winetricks flameshot --noconfirm --needed
@@ -38,7 +38,7 @@ sudo pacman -S catfish pacman-contrib gufw proxychains tor neovim pkgconf audaci
 sudo pacman -S flac yt-dlp mkvtoolnix-cli mkvtoolnix-gui steam --noconfirm --needed
 sudo pacman -S cpu-x gamemode gsmartcontrol cmatrix w3m ddgr cmus xorg-xkill podman distrobox e2fsprogs ripgrep memtest86+ tree gdu less mpg123 imagemagick tldr feh alsa-utils gptfdisk ntfs-3g os-prober python-pip --noconfirm --needed
 #-----------------------------------------------------------------
-yay -S --noconfirm dxvk-bin quickemu quickgui-bin winegui-bin input-remapper-git ttf-ms-fonts
+yay -S --noconfirm dxvk-bin winegui-bin input-remapper-git ttf-ms-fonts
 #yay -S --noconfirm konsave
 flatpak install --noninteractive flathub com.heroicgameslauncher.hgl && flatpak install --noninteractive flathub io.gitlab.librewolf-community && flatpak install --noninteractive flathub com.github.tchx84.Flatseal && flatpak install --noninteractive flathub net.mediaarea.MediaInfo && flatpak install --noninteractive flathub org.torproject.torbrowser-launcher && flatpak install --noninteractive flathub io.github.giantpinkrobots.varia
 flatpak install --noninteractive flathub com.calibre_ebook.calibre && flatpak install --noninteractive flathub com.github.Matoking.protontricks && flatpak install --noninteractive flathub app.zen_browser.zen
