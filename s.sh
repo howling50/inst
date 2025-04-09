3f07f82960330c6dc37e2cbd6bd0537f2ce62870
howling50 <95032680+howling50@users.noreply.github.com>
2025-04-10 00:01:07 +0300

    Update s.sh

--- s.sh
+++ s.sh
@@ -86,6 +86,7 @@ fi
 #defaults,ssd,noatime,compress=zstd:3,space_cache=v2 0 0
 #snapshots = defaults,ssd,noatime,compress=no,space_cache=v2 0 0
 #ext4 = defaults,noatime,barrier=1,data=ordered,errors=remount-ro,commit=60,nofail 0 2
+#echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.profile
 #sudo zypper install -y -n xhost && xhost +local: or xhost +si:localuser:$USER
 #sudo zypper install -y -n conky && cp ~/Downloads/inst/conky.desktop ~/.config/autostart/conky.desktop && cp ~/Downloads/inst/.conkyrc ~/.conkyrc && conky -c ~/.conkyrc &
 #zypper in mirrorsorcerer && systemctl enable --now mirrorsorcerer  xfce4-i3-workspaces-plugin  (patterns-games-games patterns-kde-kde_pim sudo zypper dup --from vlc --allow-vendor-change -y) (about:profiles, open root profile folder,Clear start up cache" ) sestatus  and /etc/selinux/config , Exec=/usr/bin/xdg-su -c /sbin/yast2  system-config-printer
