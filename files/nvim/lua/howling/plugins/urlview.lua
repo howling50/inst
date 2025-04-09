95724c9705a4f60d7985fa1e83ca89ce1da0699c
howling50 <95032680+howling50@users.noreply.github.com>
2025-04-09 14:31:12 +0300

    Delete files/nvim/lua/howling/plugins/urlview.lua

--- files/nvim/lua/howling/plugins/urlview.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "axieax/urlview.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim" }, -- Optional for Telescope integration
-    config = function()
-        require("urlview").setup({})
-    end
-}
