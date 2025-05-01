b8f33f2a34c9e8f4fca3e257344c965876b2eca8
howling50 <95032680+howling50@users.noreply.github.com>
2025-05-01 15:30:07 +0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -22,8 +22,6 @@ sudo btrfs subvol create /Swap && sudo chattr +C /Swap && sudo swapoff -a && sud
 #-------
 sudo btrfs subvol create /Media && sudo chown $(whoami):$(whoami) /Media && sudo chmod 755 /Media && mkdir -p ~/.config/qBittorrent && mkdir -p ~/Media && mkdir -p ~/.wine && sudo mkdir -p /var/lib/flatpak && mkdir -p ~/.local/share/flatpak && sudo chattr -R +C ~/Media && sudo chattr -R +C ~/.wine
 #---
-#curl -O https://blackarch.org/strap.sh && chmod +x strap.sh && sudo ./strap.sh
-#---
 #sudo pacman -S system-config-printer cups --noconfirm --needed && sudo systemctl enable --now cups.service cups.socket cups.path
 #sudo systemctl stop cups && sudo systemctl disable cups.service cups.socket cups.path
 #balooctl disable && sudo rm -rf ~/./local/share/baloo
@@ -117,7 +115,13 @@ fi
 #GRUB_DEFAULT=saved
 #GRUB_SAVEDEFAULT=true
 #GRUB_DISABLE_SUBMENU=y
-#---
+#----cachy if you want---
+#cachyosrepo install = curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz && tar xvf cachyos-repo.tar.xz && cd cachyos-repo && sudo ./cachyos-repo.sh
+#cachyosrepo uninst = curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz && tar xvf cachyos-repo.tar.xz && cd cachyos-repo&& sudo ./cachyos-repo.sh --remove
+#cachyoskernel = yay -S linux-cachyos linux-cachyos-headers && sudo mkinitcpio -P && sudo grub-mkconfig -o /boot/grub/grub.cfg
+#---blackarch repo---
+#curl -O https://blackarch.org/strap.sh && chmod +x strap.sh && sudo ./strap.sh
+#---------------
 #sudo grub-mkconfig -o /boot/grub/grub.cfg
 #sudo pacman -S  i3-wm polybar python-i3ipc autotiling rofi rofi-calc i3lock gvfs-mtp gvfs-afc pamixer mpv-mpris dunst baobab numlockx pavucontrol tumbler polkit-gnome unzip htop jq xfce4-terminal xfce4-taskmanager imagemagick thunar thunar-volman thunar-archive-plugin gvfs lxappearance  --noconfirm --needed && chmod +x ~/.config/polybar/launch.sh
 #/etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm.conf or /etc/sddm.conf [Autologin] User=howling Session=i3   other: pika-backup
