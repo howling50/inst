be1a04bedfc348a3080b578b58ad5254a5a0c643
howling50 <95032680+howling50@users.noreply.github.com>
2025-05-06 00:52:56 +0300

    Update keybind.sh

--- files/i3/scripts/keybind.sh
+++ files/i3/scripts/keybind.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
 config_file=~/.config/i3/config
-keybinds=$(grep -oP '(?<=bindsym ).*' "$config_file")
 
-keybinds=$(echo "$keybinds" | sed -E '
-  s/,//g;                  # Remove all commas
-  s/exec +//g;             # Remove "exec" and following space
-  s/^([^ ]+) /\1 = /;     # Add "=" after key combination
-  s/Mod1/Alt/g;            # Replace Mod1 with Alt
-  s/\$mod/SUPER/g;         # Replace $mod with SUPER
-  s/ += +/ = /g;           # Clean up extra spaces around =
+# Get display lines and original commands in parallel
+display_lines=$(grep -oP '(?<=bindsym ).*' "$config_file" | sed -E '
+  s/\$mod/SUPER/g;
+  s/Mod1/Alt/g;
+  s/,( exec|$)//g;
+  s/^([^ ]+) (.*)$/\1 = \2/;
+  s/ = exec / = /;
 ')
 
-rofi -dmenu -p "Keybinds" <<< "$keybinds"
+# Get original commands (without bindsym) into array
+mapfile -t execute_commands < <(grep -oP '(?<=bindsym ).*' "$config_file" | sed -E 's/^[^ ]+ //')
+
+# Show Rofi and get selection index
+selected_index=$(echo "$display_lines" | rofi -dmenu -p "Keybinds" -format i)
+
+# Execute corresponding command if valid
+if [[ -n "$selected_index" ]]; then
+    original_cmd="${execute_commands[$selected_index]}"
+    if [[ "$original_cmd" == exec\ * ]]; then
+        eval "${original_cmd#exec }" &
+    else
+        i3-msg "$original_cmd" &
+    fi
+fi
