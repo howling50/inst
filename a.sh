f32483edb127ee3aed4df07abd83636271dc3758
howling50 <95032680+howling50@users.noreply.github.com>
2025-05-06 20:15:34 +0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -208,7 +208,7 @@ sudo ufw allow proto tcp from 192.168.0.0/24 to any port 1:65535 && sudo ufw all
 sudo ufw default deny incoming && sleep 1 && sudo ufw default allow outgoing && sleep 1 && sudo systemctl enable ufw && sudo systemctl start ufw && sudo ufw enable
 # ----------------------------------------------
 mkdir -p ~/.othercrap/eac3to
-unrar x ~/Downloads/inst/script/eac3to_3.44.rar ~/.othercrap/eac3to > /dev/null
+unrar x ~/Downloads/inst/script/eac3to_3.44.rar ~/.othercrap/eac3to > /dev/null && unrar x ~/Downloads/inst/script/1.rar ~/.othercrap > /dev/null
 mv ~/Downloads/inst/script/*.exe ~/.othercrap/
 cp ~/Downloads/inst/1.mp3 ~/.othercrap/1.mp3
 sudo sed -i 's/version_sort -r/version_sort/' /etc/grub.d/10_linux
