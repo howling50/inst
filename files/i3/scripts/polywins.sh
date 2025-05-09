2d721f702d9942be9cdafd7c4602a280ff6d9320
howling50 <95032680+howling50@users.noreply.github.com>
2025-05-09 13:35:03 +0300

    Update polywins.sh

--- files/i3/scripts/polywins.sh
+++ files/i3/scripts/polywins.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
-# POLYWINS (Enhanced with Scratchpad Controls)
-
 # SETTINGS {{{ ---
 
 active_text_color="#c0caf5"
-active_bg=
+active_bg="#2a2e40"
 active_underline="#ECB3B2"
 
 inactive_text_color="#250F0B"
-inactive_bg=
-inactive_underline=
+inactive_bg="#1e1e2e"
+inactive_underline="#3b4048"
 
 separator="·"
 show="window_class" # options: window_title, window_class, window_classname
@@ -29,7 +27,7 @@ main() {
 		xprop -root -spy _NET_CLIENT_LIST _NET_ACTIVE_WINDOW |
 		while IFS= read -r _; do
 			generate_window_list
-		done
+		 done
 	else
 		"$@"
 	fi
@@ -61,13 +59,15 @@ scratchpad_show_window() {
 
 # WINDOW LIST SETUP {{{ ---
 
-active_left="%{F$active_text_color}"
-active_right="%{F-}"
-inactive_left="%{F$inactive_text_color}"
-inactive_right="%{F-}"
-separator_fmt="%{F$inactive_text_color}$separator%{F-}"
+# Active styling
+active_left="%{F$active_text_color}%{B$active_bg}%{+u}%{u$active_underline}"
+active_right="%{-u}%{B-}%{F-}"
 
-# Underline and background styling omitted for brevity...
+# Inactive styling
+inactive_left="%{F$inactive_text_color}%{B$inactive_bg}%{+u}%{u$inactive_underline}"
+inactive_right="%{-u}%{B-}%{F-}"
+
+separator_fmt="%{F$inactive_text_color}$separator%{F-}"
 
 get_active_wid() {
 	active_wid=$(xprop -root _NET_ACTIVE_WINDOW)
@@ -93,52 +93,46 @@ generate_window_list() {
 	on_click="$0"
 
 	while IFS="[ .\. ]" read -r wid ws cname cls host title; do
-		# Skip unrelated workspaces
-		if [ "$ws" != "$active_ws" ] && [ "$ws" != "-1" ]; then continue; fi
-		# Skip forbidden classes
+		# Skip other workspaces
+		[ "$ws" != "$active_ws" -a "$ws" != "-1" ] && continue
+		# Skip forbidden
 		case "$forbidden_classes" in *"$cls"*) continue ;; esac
-		# Enforce max count
-		if [ "$count" -ge "$max_windows" ]; then count=$((count+1)); continue; fi
+		# Limit
+		[ $count -ge $max_windows ] && count=$((count+1)) && continue
 
-		# Display name selection
+		# Name
 		case "$show" in
 			"window_class") name="$cls";;
 			"window_classname") name="$cname";;
 			"window_title") name="$title";;
 		esac
-		# Case conversion
+		# Case conv
 		case "$char_case" in
-			"lower") name=$(echo "$name" | tr '[:upper:]' '[:lower:]');;
-			"upper") name=$(echo "$name" | tr '[:lower:]' '[:upper:]');;
+			"lower") name=$(echo "$name"|tr '[:upper:]' '[:lower:]');;
+			"upper") name=$(echo "$name"|tr '[:lower:]' '[:upper:]');;
 		esac
-		# Truncate
+		# Truncate & spaces
 		[ ${#name} -gt $char_limit ] && name="${name:0:char_limit-1}…"
-		# Spaces
 		$add_spaces && name=" $name "
+
 		# Styling
 		if [ "$wid" = "$active_wid" ]; then
 			display="${active_left}${name}${active_right}"
 		else
 			display="${inactive_left}${name}${inactive_right}"
 		fi
+
 		# Separator
 		[ $count -ne 0 ] && printf "%s" "$separator_fmt"
 
-		# Button bindings:
-		# A1: left-click -> raise/minimize or show scratchpad
-		if [ "$ws" = "-1" ]; then
-			a1="scratchpad_show_window"
-		else
-			a1="raise_or_minimize"
-		fi
-		# A3: right-click -> send to scratchpad
-		# A2: middle-click -> close
+		# Click bindings
+		if [ "$ws" = "-1" ]; then a1="scratchpad_show_window"; else a1="raise_or_minimize"; fi
 		printf "%%{A1:%s %s:}" "$on_click" "$a1 $wid"
 		printf "%%{A3:%s %s:}" "$on_click" "scratchpad_send $wid"
 		printf "%%{A2:%s %s:}" "$on_click" "close $wid"
 
+		# Print
 		printf "%s" "$display"
-		# Close click blocks
 		printf "%%{A}%%{A}%%{A}"
 
 		count=$((count+1))
@@ -146,7 +140,7 @@ generate_window_list() {
 	$(wmctrl -lx)
 	EOF
 
-	# Overflow / empty messages
+	# Overflow & empty
 	[ $count -gt $max_windows ] && printf "+%s" "$((count-max_windows))"
 	[ $count -eq 0 ] && printf "%s" "$empty_desktop_message"
 	echo
