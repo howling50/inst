8a2f4227be9d2ac89ae47059dd279e20fb9d271f
howling50 <95032680+howling50@users.noreply.github.com>
2025-05-26 20:28:12 +0300

    Update lazy.lua

--- files/nvim/lua/howling/lazy.lua
+++ files/nvim/lua/howling/lazy.lua
@@ -11,7 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "howling.plugins" }, { import = "howling.plugins.lsp" } }, {
+ -- require("lazy").setup({ { import = "howling.plugins" }, { import = "howling.plugins.lsp" } }, {
+ require("lazy").setup({ { import = "howling.plugins" } }, { 
   checker = {
     enabled = true,
     notify = false,
