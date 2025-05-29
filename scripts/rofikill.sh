dbd33f7c1882157bdafbd5196b6385ce881eea99
howling50 <95032680+howling50@users.noreply.github.com>
2025-05-29 19:18:11 +0300

    Update rofikill.sh

--- scripts/rofikill.sh
+++ scripts/rofikill.sh
@@ -1,3 +1,31 @@
 #!/usr/bin/env bash
-selected=$(ps --no-headers -u "$USER" -o pid,comm,%cpu,%mem | rofi -dmenu -i -c -l 10 -p "Kill process:" -theme ~/.config/rofi/basiceffect.rasi)
-[ -n "$selected" ] && echo "$selected" | awk '{print $1}' | xargs -r kill
+
+# List user processes and show in rofi
+selected=$(ps --no-headers -u "$USER" -o pid,comm,%cpu,%mem | 
+    rofi -dmenu -i -c -l 10 -p "Kill process:" -theme ~/.config/rofi/basiceffect.rasi)
+
+# Exit if nothing selected
+[ -z "$selected" ] && exit 0
+
+# Extract PID and command name
+pid=$(echo "$selected" | awk '{print $1}')
+cmd=$(echo "$selected" | awk '{print $2}')
+
+# Confirmation menu with default option
+action=$(printf "Selected (PID %s)\nAll Instances (%s)" "$pid" "$cmd" | 
+    rofi -dmenu -i -c -l 2 -p "Kill method:" -theme ~/.config/rofi/basiceffect.rasi -selected-row 0)
+
+# Execute based on user choice
+case "$action" in
+    "Selected (PID $pid)")
+        kill "$pid"
+        notify-send "Process killed" "PID: $pid ($cmd)"
+        ;;
+    "All Instances ($cmd)")
+        pkill -x "$cmd"
+        notify-send "All instances killed" "Command: $cmd"
+        ;;
+    *) 
+        exit 0
+        ;;
+esac
