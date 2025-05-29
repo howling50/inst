71ae4e04b1fe9377db5c9907570ee837e0e71c0a
howling50 <95032680+howling50@users.noreply.github.com>
2025-05-29 18:31:46 +0300

    Update output_switch.sh

--- scripts/output_switch.sh
+++ scripts/output_switch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pactl set-default-sink "$(pactl list short sinks | awk '{print $2}' | rofi -dmenu -i -l 5 -c -p "Output:" -theme ~/.config/rofi/launcher.rasi)" && notify-send "Audio switched!" || exit 0
+pactl set-default-sink "$(pactl list short sinks | awk '{print $2}' | rofi -dmenu -i -l 5 -c -p "Output:" -theme ~/.config/rofi/basiceffect.rasi)" && notify-send "Audio switched!" || exit 0
